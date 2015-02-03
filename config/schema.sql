58ebd27ab4152597ab1a0dae93e9da09d3c260ca
jluchiji <jluchiji@purdue.edu>
2015-02-02 21:55:56 -0500

    Fixed a few mySQL specific problems
    …only worked with sqlite, so this was expected.

--- config/schema.sql
+++ config/schema.sql
@@ -4,33 +4,34 @@
 # Copyright © Contributors. See docs/charter.md for team roster.              #
 #                                                                             #
 # =========================================================================== #
-
-CREATE TABLE IF NOT EXISTS packages (
-  `id`          TEXT      NOT NULL,
-  `tracking`    TEXT      NOT NULL,
-  `received`    DATETIME  NOT NULL,
-  `recipient`   TEXT      NOT NULL,
-  `released`    DATETIME  DEFAULT NULL,
-  `user`        TEXT      NOT NULL,
+CREATE TABLE IF NOT EXISTS users (
+  `id`          VARCHAR(20) NOT NULL,
+  `first_name`  VARCHAR(50) NOT NULL,
+  `last_name`   VARCHAR(50) NOT NULL,
+  `uname`       VARCHAR(50) NOT NULL  UNIQUE,
+  `hash`        TEXT        NOT NULL,
+  `auth`        TEXT        NOT NULL,
   PRIMARY KEY (id),
-  INDEX packages_tracking (tracking),
-  FOREIGN KEY (recipient) REFERENCES recipients(id) ON DELETE CASCADE,
-  FOREIGN KEY (user) REFERENCES users(id) ON DELETE NO ACTION
+  INDEX users_uname (uname)
 );
 
 CREATE TABLE IF NOT EXISTS recipients (
-  `id`          TEXT      NOT NULL      PRIMARY KEY,
-  `first_name`  TEXT      NOT NULL,
-  `last_name`   TEXT      NOT NULL,
+  `id`          VARCHAR(20) NOT NULL,
+  `first_name`  VARCHAR(50) NOT NULL,
+  `last_name`   VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id),
   INDEX recipients_name (first_name, last_name)
 );
 
-CREATE TABLE IF NOT EXISTS users (
-  `id`          TEXT      NOT NULL      PRIMARY KEY,
-  `first_name`  TEXT      NOT NULL,
-  `last_name`   TEXT      NOT NULL,
-  `uname`       TEXT    NOT NULL  UNIQUE,
-  `hash`        TEXT    NOT NULL,
-  `auth`        TEXT    NOT NULL,
-  INDEX users_uname (uname)
+CREATE TABLE IF NOT EXISTS packages (
+  `id`          VARCHAR(20) NOT NULL,
+  `tracking`    VARCHAR(100) NOT NULL,
+  `received`    DATETIME    NOT NULL,
+  `recipient`   VARCHAR(20) NOT NULL,
+  `released`    DATETIME    DEFAULT NULL,
+  `user`        VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id),
+  INDEX packages_tracking (tracking),
+  FOREIGN KEY (recipient) REFERENCES recipients(id) ON DELETE CASCADE,
+  FOREIGN KEY (user) REFERENCES users(id) ON DELETE SET NULL
 );
